c57b021f8ce73e3e551a6f98087921b49488b05d
Mr.UtbahIrfan <utbah26199@gmail.com>
2023-11-24 18:07:30 +0500

    Update - Splash - GeStarted

---
+++ app/src/main/java/com/smlab/santaspotter/UploadPhoto.java
@@ -0,0 +1,14 @@
+package com.smlab.santaspotter;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UploadPhoto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_photo);
+    }
+}
