8aebc493d4c869f94539c47cdb94ede7df27f3a8
Mr.UtbahIrfan <utbah26199@gmail.com>
2023-11-27 20:23:09 +0500

    Update - Select Santa Adapter - SelectSanta Model - Edit Santa

--- app/src/main/java/com/smlab/santaspotter/GetStarted.java
+++ app/src/main/java/com/smlab/santaspotter/GetStarted.java
@@ -2,7 +2,13 @@ package com.smlab.santaspotter;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.util.Linkify;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -10,6 +16,7 @@ import android.view.animation.DecelerateInterpolator;
 import android.view.animation.ScaleAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -20,6 +27,7 @@ public class GetStarted extends AppCompatActivity {
     ConstraintLayout constraintLayout2;
     ImageView img_santa;
     Button button_spot_him;
+    TextView urlTextView;
 
 
     @SuppressLint("MissingInflatedId")
@@ -32,6 +40,26 @@ public class GetStarted extends AppCompatActivity {
         img_santa = findViewById(R.id.getStarted_logo);
         button_spot_him = findViewById(R.id.button_spot_him);
         constraintLayout2 = findViewById(R.id.started_constraint_2);
+        urlTextView = findViewById(R.id.url_textView);
+
+        // for URL to go website
+        SpannableString spannableString = new SpannableString("https://www.copperfield.com");
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View textView) {
+                // Handle link click action here
+                // For example, open the URL in a browser
+                Uri uri = Uri.parse("https://www.copperfield.com");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        };
+        spannableString.setSpan(clickableSpan, 0, spannableString.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        urlTextView.setText(spannableString);
+        urlTextView.setMovementMethod(LinkMovementMethod.getInstance());
+        // Set the link text color
+        urlTextView.setLinkTextColor(getResources().getColor(R.color.red_text_color));
 
         button_spot_him.setOnClickListener(new View.OnClickListener() {
             @Override
