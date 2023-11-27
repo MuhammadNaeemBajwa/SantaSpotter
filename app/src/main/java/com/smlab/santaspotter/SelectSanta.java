dadf21773141cca51f7d0683466ba28cd5a9aeff
Naeem bajwax <naeem.bajwax@gmail.com>
2023-11-27 20:23:16 +0500

    update - Select Santa Adapter

--- app/src/main/java/com/smlab/santaspotter/SelectSanta.java
+++ app/src/main/java/com/smlab/santaspotter/SelectSanta.java
@@ -1,5 +1,7 @@
 package com.smlab.santaspotter;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -8,36 +10,56 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.smlab.santaspotter.databinding.ActivityAddSantaBinding;
+import com.smlab.santaspotter.databinding.ActivitySelectSantaBinding;
+
 import java.util.ArrayList;
 
-public class SelectSanta extends AppCompatActivity {
+public class SelectSanta extends AppCompatActivity implements SelectSantaAdapter.OnItemClickListener {
     ConstraintLayout includePickMe;
     RecyclerView recyclerView;
     SelectSantaAdapter adapter;
     ArrayList<SelectSantaModel> selectSantaList;
+    Dialog dialogCode;
+    private ActivitySelectSantaBinding binding;
+    private ImageView selectedSantaSticker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_select_santa);
+        binding = ActivitySelectSantaBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setIds();
+        initialized();
+        setUpRecyclerView();
+
+    }
 
 
+    private void setIds() {
         includePickMe = findViewById(R.id.pick_me_include);
         recyclerView = findViewById(R.id.recyclerView2);
-
         includePickMe.findViewById(R.id.pick_me_button).setOnClickListener(view -> showCodeDialog());
-
         includePickMe.findViewById(R.id.back_arrow).setOnClickListener(view -> onBackPressed());
 
-        selectSantaList = new ArrayList<>();
+        selectedSantaSticker = binding.pickMeInclude.selectSantaSticker;
 
+    }
+
+    private void initialized() {
+        selectSantaList = new ArrayList<>();
+    }
+
+    private void setUpRecyclerView() {
         selectSantaList.add(new SelectSantaModel(R.drawable.santa_sticker));
         selectSantaList.add(new SelectSantaModel(R.drawable.santa_sticker_1));
         selectSantaList.add(new SelectSantaModel(R.drawable.santa_img));
@@ -49,12 +71,14 @@ public class SelectSanta extends AppCompatActivity {
         recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
         adapter = new SelectSantaAdapter(SelectSanta.this, selectSantaList);
         recyclerView.setAdapter(adapter);
-
-
     }
-
-    Dialog dialogCode;
-
+    @Override
+    public void onItemClick(int position) {
+        SelectSantaModel selectedSanta = selectSantaList.get(position);
+        Log.d(TAG, "onItemClick: " + selectedSanta);
+        selectedSantaSticker.setImageResource(selectedSanta.getSantaSticker());
+        Log.d(TAG, "onItemClick: getSantaSticker:" + selectedSanta.getSantaSticker());
+    }
     private void showCodeDialog() {
         dialogCode = new Dialog(SelectSanta.this, R.style.dialog);
         dialogCode.setContentView(R.layout.unlock_popup_dialog);
@@ -79,20 +103,18 @@ public class SelectSanta extends AppCompatActivity {
         dontHaveCode.setOnClickListener(view -> showAccessCodeDialog());
         dialogCode.show();
 
-
     }
 
     private void showAccessCodeDialog() {
         Dialog dialog = new Dialog(SelectSanta.this, R.style.dialog);
         dialog.setContentView(R.layout.access_code_dialog);
-
         ImageView close;
         close = dialog.findViewById(R.id.imageView_cross);
         close.setOnClickListener(view -> dialog.dismiss());
-
         dialog.show();
-
         dialogCode.dismiss();
 
     }
+
+
 }
