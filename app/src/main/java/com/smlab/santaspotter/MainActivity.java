fdb25c0f66f5c210b68f0b6fa6d9a8b685ec2457
Naeem bajwax <naeem.bajwax@gmail.com>
2023-11-27 14:28:43 +0500

    update - Sticker's Temperature & Brightness

--- app/src/main/java/com/smlab/santaspotter/MainActivity.java
+++ app/src/main/java/com/smlab/santaspotter/MainActivity.java
@@ -2,13 +2,65 @@ package com.smlab.santaspotter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.ColorFilter;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.os.Bundle;
+import android.widget.SeekBar;
+
+import com.smlab.santaspotter.databinding.ActivityMainBinding;
+import com.smlab.santaspotter.filter.ColorFilterGenerator;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        updateBrightnessColorFilter(binding.seekBar.getProgress());
+
+        // Set up a listener for the SeekBar changes
+        binding.seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+//                Nov 27, 2023  -   Both Brightness and Temperature Color Filter is perfectly fine.
+                //                  updateBrightnessColorFilter(progress);
+                updateTemperatureColorFilter(progress);
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
     }
+
+    private void updateBrightnessColorFilter(int progress) {
+        // Adjust the brightness based on the progress and set the color filter
+
+//        Nov 27, 2023  -   Brightness functionality is perfectly working.
+
+//
+//        binding.santaSticker.setColorFilter(
+//                ColorFilterGenerator.adjustBrightness(progress)
+//        );
+
+    }
+
+    private void updateTemperatureColorFilter(int progress) {
+
+//        Nov 27, 2023  -   Temperature functionality is perfectly fine
+
+        // Update the ColorFilter based on the temperature progress
+        ColorFilter temperatureFilter = ColorFilterGenerator.adjustTemperatureUpdate(progress);
+
+        // Apply the ColorFilter to your ImageView or any other view
+        binding.santaSticker.setColorFilter(temperatureFilter);
+    }
+
 }
